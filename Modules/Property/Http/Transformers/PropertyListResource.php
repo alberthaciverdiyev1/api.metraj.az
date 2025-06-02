a529a15793d97a153f1e474067a4f887cb664c73
Albert <alberthaciverdiyev55@gmail.com>
2025-06-02 17:42:57 +0400

    added random images for test

--- Modules/Property/Http/Transformers/PropertyListResource.php
+++ Modules/Property/Http/Transformers/PropertyListResource.php
@@ -4,6 +4,7 @@ namespace Modules\Property\Http\Transformers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 
 class PropertyListResource extends JsonResource
 {
@@ -12,107 +13,30 @@ class PropertyListResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $images = [
+            'https://media.istockphoto.com/id/1390233984/photo/modern-luxury-bedroom.jpg?s=612x612&w=0&k=20&c=po91poqYoQTbHUpO1LD1HcxCFZVpRG-loAMWZT7YRe4=',
+            'https://assets.architecturaldigest.in/photos/621f43c6fa7af6911e95b64a/16:9/w_1280,c_limit/7%20budget-friendly%20kids%E2%80%99%20room%20design%20ideas.jpg',
+            'https://media.designcafe.com/wp-content/uploads/2023/07/05141750/aesthetic-room-decor.jpg',
+            'https://www.thespruce.com/thmb/2_Q52GK3rayV1wnqm6vyBvgI3Ew=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/put-together-a-perfect-guest-room-1976987-hero-223e3e8f697e4b13b62ad4fe898d492d.jpg',
+            'https://www.brightonhomes.net.au/sites/default/files/styles/blog_hero_banner/public/what-is-a-rumpus-room.jpg?itok=Df0VIb-y',
+            'https://www.realsimple.com/thmb/oO5rVtxCPEdBMVmVRLN0Ag-uELY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/LivingRoom_0288-real-simple-home-2023-0923fea-1fbd00fca003419b8ef5bdb11d95187c.jpg',
+            'https://www.gentinghotel.co.uk/_next/image?url=https%3A%2F%2Fs3.eu-west-2.amazonaws.com%2Fstaticgh.gentinghotel.co.uk%2Fuploads%2Fhero%2FSuiteNov2022_0008_1920.jpg&w=3840&q=75',
+            'https://www.ikea.com/images/a-shared-childrens-room-with-two-beds-where-some-mammut-chil-548e7eb28566d8edb960f3297cc57e09.jpg?f=xl',
+            'https://mustardmade.com/cdn/shop/articles/Colourful_kids_rooms_1200x.png?v=1713928416',
+            'https://rnb.scene7.com/is/image/roomandboard/arro_529831_24e?size=2400,2400&scl=1',
+            'https://media.designcafe.com/wp-content/uploads/2020/05/17174742/childrens-bedroom-with-bunk-bed-and-seating-area.jpg'
+        ];
         return [
             'id' => $this->getAttribute('id'),
-            'name' => $this->getAttribute('name'),
             'slug' => $this->getAttribute('slug'),
             'date' => \Carbon\Carbon::parse($this->getAttribute('date'))->format('d F, Y'),
-            'image' => 'https://themesflat.co/html/proty/images/section/box-house-16.jpg',
-            'title' => $this->number_of_rooms . $this->subway->name ?? $this->district->name,
-
-            'address' => '102 Ingraham St, Brooklyn, NY 11237',
+            'image' => Arr::random($images),
+            'title' => $this->number_of_rooms .' otaqlı '. $this->subway->name ?? $this->district->name,
+            'address' => $this->getAttribute('address'),
             'beds' => 4,
             'baths' => 3,
-            'area' => '4,043',
+            'area' => $this->getAttribute('area'),
             'price' => '8,600',
-            'video' => 'https://www.youtube.com/embed/MLpWrANjFbI',
-
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam risus leo, blandit vitae diam a, vestibulum viverra nisi. Vestibulum ullamcorper velit eget mattis aliquam. Proin dapibus luctus pulvinar. Integer et libero ut purus bibendum gravida non ac tellus.
-
-Aliquam non lorem consequat, luctus dui et, auctor nisi. Aenean placerat sapien at augue lacinia, non semper urna tempor. Mauris sit amet elit orci',
-            'altimages' => [
-                'first_image' => 'webui/images/property-detail-3.jpg',
-                'second_image' => 'webui/images/property-detail-4.jpg',
-                'third_image' => 'webui/images/property-detail-5.jpg',
-                'fourth_image' => 'webui/images/property-detail-6.jpg'
-            ],
-            'details' => [
-                'units' => '3 Units in North Hollywood with upside potential through construction of an ADU (buyer to verify).',
-                'unit_mix' => '(3) 3+1 bath units',
-                'building_size' => '2,660 sqft',
-                'lot_size' => '6,001 sqft',
-                'access' => 'Easy access to the 101, 170, and 134 freeways.',
-                'metering' => 'Separately metered for gas and electricity',
-            ],
-            'extra' => [
-                'id' => '1234',
-                'price_text' => '$7,500',
-                'size' => '150 sqft',
-                'rooms' => 9,
-                'beds_exact' => 7.328,
-                'year_built' => 2022,
-                'type' => 'Villa',
-                'status' => 'For sale',
-                'garage' => 3,
-                'rent_price' => '$250,00 /month',
-            ],
-            'amenities' => [
-                'Smoke alarm',
-                'Carbon monoxide alarm',
-                'First aid kit',
-                'Self check-in with lockbox',
-                'Security cameras',
-                'Hangers',
-                'Bed linens',
-                'Extra pillows & blankets',
-                'Iron',
-                'TV with standard cable',
-                'Refrigerator',
-                'Microwave',
-                'Dishwasher',
-                'Coffee maker',
-            ],
-
-
-            'floor_plan' => [
-                [
-                    'floor' => 'First Floor',
-                    'bedrooms' => 2,
-                    'bathrooms' => 3,
-                    'image' => 'https://themesflat.co/html/proty/images/section/floor.jpg',
-                ],
-                [
-                    'floor' => 'Second Floor',
-                    'bedrooms' => 1,
-                    'bathrooms' => 5,
-                    'image' => 'https://themesflat.co/html/proty/images/section/floor.jpg',
-                ],
-            ],
-
-            'nearby' => [
-
-                'School' => '0.7 km',
-                'University' => '1.3 km',
-                'Grocery center' => '0.6 km',
-                'Market' => '1.1 km',
-            ],
-            'map' => [
-                'latitude' => 40.5812895,
-                'longitude' => 49.6735533,
-                'address' => 'Sumgait beach',
-                'city' => 'Sumgait',
-                'state' => 'Absheron',
-                'postal_code' => 'AZ5000',
-                'area' => 7345,
-                'country' => 'Azerbaijan',
-            ],
-            'virtual_tour' => [
-                'link' => 'https://www.youtube.com/embed/MLpWrANjFbI',
-                'image' => 'https://themesflat.co/html/proty/images/section/property-detail-2.jpg',
-            ],
-            'comments' => [],
-
-
         ];
     }
 }
