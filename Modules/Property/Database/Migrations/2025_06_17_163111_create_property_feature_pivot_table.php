47e9da76e9ee95943eff91b7d8320c7ddffe84aa
Albert <alberthaciverdiyev55@gmail.com>
2025-06-19 02:21:31 +0400

    added primary key to property feature table

--- Modules/Property/Database/Migrations/2025_06_17_163111_create_property_feature_pivot_table.php
+++ Modules/Property/Database/Migrations/2025_06_17_163111_create_property_feature_pivot_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('feature_property', function (Blueprint $table) {
             $table->foreignId('property_id')->constrained('properties')->onDelete('cascade');
             $table->foreignId('feature_id')->constrained('features')->onDelete('cascade');
+            $table->primary(['property_id', 'feature_id']);
         });
     }
 
